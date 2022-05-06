b884f82aa167acd53dd150076c9358af5f1e5d28
caio-magnani <49353875+caio-magnani@users.noreply.github.com>
2022-05-05 21:10:04 -0300

    Update Student.cs

--- UniversityProject/UniversityProject/Models/Student.cs
+++ UniversityProject/UniversityProject/Models/Student.cs
@@ -8,45 +8,8 @@ using System.Threading.Tasks;
 
 namespace UniversityProject.Models
 {
-    public class Student
+    public class Student : NaturalPersonUser
     {
-        [Key]
-        public int Id { get; set; }
-
-        [Column(TypeName = "int")]
-        [DisplayName("Número do curso")]
-        public int IdCurso { get; set; }
-
-        
-        [DisplayName("Número do usuário")]
-        [Column(TypeName = "int")]
-        public int IdUsuario { get; set; }
-
-        [DisplayName("Nome")]
-        [Required(ErrorMessage = "Este campo é obrigatório")]
-        [Column(TypeName = "nvarchar(250)")]
-        public string Nome { get; set; }
-
-        [DisplayName("Matrícula")]
-        [Column(TypeName = "varchar(50)")]
-        public string Matricula { get; set; }
-
-        [DisplayName("CPF")]
-        [Required(ErrorMessage = "Este campo é obrigatório")]
-        [Column(TypeName = "varchar(50)")]
-        public string Cpf { get; set; }
-
-        [DisplayName("Email")]
-        [Required(ErrorMessage = "Este campo é obrigatório")]
-        [Column(TypeName = "varchar(100)")]
-        public string Email { get; set; }
-
-        //public List<Subject> Subjects
-        //{
-        //    get { return Subjects; }
-        //}
-        //public List<Turma>
-
-
+        public override int permission() => 1;
     }
 }
