3aecea358e38cc37019fd9dd47c5208bb9f86211
caio-magnani <49353875+caio-magnani@users.noreply.github.com>
2022-05-05 21:19:07 -0300

    Update

--- UniversityProject/UniversityProject/Models/DataBase.cs
+++ UniversityProject/UniversityProject/Models/DataBase.cs
@@ -9,14 +9,13 @@ namespace UniversityProject.Models
 {
     public class DataBase : DbContext
     {
-        public DbSet<User> Users { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<Partner> Partner { get; set; }
         public string connectionString { get; set; } = @"Data Source=.\SQLExpress;Database=UniversityProjectLab;integrated Security=true"; 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString: @"Data Source=.\SQLExpress;Database=UniversityProjectLab;integrated Security=true");
         }
-        public DbSet<UniversityProject.Models.Teacher> Teacher { get; set; }
-        public DbSet<UniversityProject.Models.Courses> Courses { get; set; }
+        
     }
 }
